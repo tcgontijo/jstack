0a05c4a69644b4179f216ec468908e56d9a3c698
tcgotijo <tulio_gontijo@hotmail.com>
2023-04-11 10:21:05 -0300

    [Primeiros passos no Node.js] Recebendo parâmetros na URL

--- firstapi/src/controllers/UserController.js
+++ firstapi/src/controllers/UserController.js
@@ -12,5 +12,21 @@ module.exports = {
     })
     response.writeHead(200, { 'Content-Type': 'application/json' });
     response.end(JSON.stringify(users));
+  },
+
+
+  getUserById(request, response) {
+    const { id } = request.params;
+
+    const user = users.find(user => user.id === Number(id));
+
+    if (!user) {
+      response.writeHead(400, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify({ error: "user not found" }));
+    } else {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(user));
+    }
+
   }
 }
